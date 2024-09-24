21b8f4ac0d04cbae9fa26e50192f7c5917c821ce
FekherJ <fekher.jarrar2@gmail.com>
2024-09-24 23:31:47 +0200

    major updates

--- test/InsurancePolicy.test.cjs
+++ test/InsurancePolicy.test.cjs
@@ -15,21 +15,24 @@ describe("InsurancePolicy with Mock Chainlink", function () {
         const MockLinkToken = await ethers.getContractFactory("MockLinkToken");
         mockLinkToken = await MockLinkToken.deploy();
         await mockLinkToken.waitForDeployment();
-        console.log('mockLinkToken address:',mockLinkToken.getAddress());
+        const mockLinkTokenAddress = await mockLinkToken.getAddress();
+        console.log('mockLinkToken address:',mockLinkTokenAddress);
         
 
         // Deploy the MockOracle contract
         const MockOracle = await ethers.getContractFactory("MockOracle");
         mockOracle = await MockOracle.deploy(mockLinkToken.getAddress());
         await mockOracle.waitForDeployment();
-        console.log('mockOracle address:',mockOracle.getAddress());
-        const MockOracleAddress = await mockOracle.getAddress
+        const MockOracleAddress = await mockOracle.getAddress();
+        console.log('mockOracle address:',MockOracleAddress);
+        
 
         // Deploy the InsurancePolicy contract
         const InsurancePolicy = await ethers.getContractFactory("InsurancePolicy");
-        insurancePolicy = await InsurancePolicy.deploy(MockOracleAddress , jobId, fee, mockLinkToken.address);
+        insurancePolicy = await InsurancePolicy.deploy(MockOracleAddress , jobId, fee, mockLinkTokenAddress);
         await insurancePolicy.waitForDeployment();
-        console.log('insurancePolicy address:',insurancePolicy.getAddress());
+        const insurancePolicyAddress = await insurancePolicy.getAddress();
+        console.log('insurancePolicy address:', insurancePolicyAddress);
     });
 
     it("Should create a policy and emit PolicyCreated event", async function () {
